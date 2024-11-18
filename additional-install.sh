ceecfa19714f233e1e7a1c203161c6293c5dc2cc
Jacob Lambert <jacob.lambert@map4.jp>
2024-11-18 10:39:30 +0900

    Update additional-install.sh

--- additional-install.sh
+++ additional-install.sh
@@ -3,4 +3,5 @@ sudo apt install -y \
    librange-v3-dev \
    ros-humble-cudnn-cmake-module \
    ros-humble-tensorrt-cmake-module \
-   ros-humble-point-cloud-msg-wrapper
+   ros-humble-point-cloud-msg-wrapper \
+   ros-humble-diagnostic-updater
