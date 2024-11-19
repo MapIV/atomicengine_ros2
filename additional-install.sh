307554136f950ac1bea3f3b7e8ef06385dcae2b3
Jacob <jacob.lambert@map4.jp>
2024-11-19 16:37:03 +0900

    cyclonedds

--- additional-install.sh
+++ additional-install.sh
@@ -4,4 +4,7 @@ sudo apt install -y \
    librange-v3-dev \
    ros-humble-cudnn-cmake-module \
    ros-humble-tensorrt-cmake-module \
-   ros-humble-point-cloud-msg-wrapper
+   ros-humble-point-cloud-msg-wrapper \
+   ros-humble-cyclonedds \
+   ros-humble-rmw-implementation \
+   ros-humble-rmw-cyclonedds-cpp
